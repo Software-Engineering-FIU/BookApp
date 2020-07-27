fd16e45cbb399c30128eb7837d0080fe419dee10
savick994 <42616093+savick994@users.noreply.github.com>
2020-07-27 19:37:36 -0400

    Update wishlist.py
    Added CreateList method

--- bookapp/src/wishlist.py
+++ bookapp/src/wishlist.py
@@ -5,7 +5,7 @@ from bookapp.dao import DatabaseAccess
 # WishList -> TableName
 # ID (unique) -> int (primary key)
 # UserID -> int (foriegn key)
-# BookID -> int
+# BookID -> int multi-value attribute/relational data
 # ShoppingID -> int
 # Name (unique) -> string
 
@@ -14,11 +14,24 @@ class Wishlist (DatabaseAccess):
     def __init__(self):
         super(Wishlist, self).__init__()
 
-    def DisplayList (self, list_id):
+    def createList (user_id, name):
+        try:
+            testQuery = ("select Name from WishList where WishList.UserID = User.ID and User.ID = '{}'").format(user_id)
+            testList = super(Wishlist, self).read(query=testQuery)
+            if name not in testList:
+                helperQuery = "select ID from ShoppingCart where ShoppingCart.UserID = User.ID and User.ID = '{}'").format(user_id)
+                cart_id = super(Wishlist, self).read(query=helperQuery)
+                query = "insert into WishList (UserID, ShoppingID, Name) values ('{}','{}','{}')".format(user_id, cart_id, name)  #create a new list, ID will auto increment, and bookID is blank until some book is added
+                super(Wishlist, self).read(query=query)
+        except Exception as e:
+            print(e, 'list with this name already exists')
+            return []
+
+    def displayList (self, list_id):
         try:
             query = ("select w.*,b.ID, title, author, publisher, price "
-                     "from wishlist as w left join books as b "
-                     "on b.ID=w.BookID where list_id='{}'").format(list_id)
+                     "from WishList as w left join books as b "
+                     "on b.ID=w.BookID where w.ID='{}'").format(list_id)
             result = super(Wishlist, self).read(query=query)
             return result
         except Exception as e:
@@ -28,11 +41,11 @@ class Wishlist (DatabaseAccess):
     def addToList (self, book_id, list_id)
         try:
             testQuery = ("select exists( w.BookID, b.ID "
-                         "from wishlist as w inner join books as b on b.ID=w.BookID"
-                         "where list_id='{}')").format(list_id)
+                         "from WishList as w inner join Book as b on b.ID=w.BookID"
+                         "where w.ID='{}')").format(list_id)
             testAdd = super(Wishlist, self).read(query=testQuery)
             if testAdd = 'true':
-                query = ("insert into wishlist (book_id) values ('{}') where list_id = '{}'").format(book_id, list_id)
+                query = ("insert into WishList (book_id) values ('{}') where WishList.ID = '{}'").format(book_id, list_id)
                 super(Wishlist, self).read(query=query)
                 # do I need a return statement?
         except Exception as e:
@@ -40,10 +53,11 @@ class Wishlist (DatabaseAccess):
             return []
 
     def removeFromList(self, book_id, list_id):
-        query = ("delete from wishlist where book_id = '{}' and list_id = '{}'")
+        query = ("delete from WishList where BookID = '{}' and ID = '{}'").format(book_id, list_id)
+        super(Wishlist, self).read(query=query)
 
-    def sendToCart(self, book_id, list_id, param):
+    def sendToCart(self, book_id, list_id, cart_id):
         removeFromList(self, book_id, list_id)
-        addToCart(self, param)
-        # @param is whatever parameter I need for the addToCart function
+        addToCart(self, book_id, cart_id)
+        # @cart_id is placeholder until addToCart function is made by Drew?
 
