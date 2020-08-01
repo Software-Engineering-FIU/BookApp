1e830a3554e8a9296272736b65c312fa0f39ccf6
abasi012 <abasi012@fiu.edu>
2020-08-01 00:18:54 -0400

    Books browsing and sorting feature. All commented, though

--- bookapp/models/book.py
+++ bookapp/models/book.py
@@ -20,8 +20,49 @@ class Book(db.Model):
     #?book and wishlist is a M-M relationship; not sure how to code M-M
     reviews = db.relationship('Review', backref='book_reviewed')    #! 1-M (D)
 
+#filter by book genre
+#@app.route('/filter_by_genre/<genre>', methods=['GET'])
+#def bookGenre(genre):
+#    book = Book.query.get_or_404(genre)
+
+#filter by top sellers
+#@app.route('/filter_by_sold/<int:sold>', methods=['GET'])
+#def getTopSold(sold):
+#    Book.query.order_by(Book.copies_sold).limit(sold)
+
+#filter by rating, need book average ratings
+#@app.route('/filter_by_rating/<int:rating>', methods=['GET'])
+#def bookRating(rating):
+#    Book.query()
+
+#@app.route('/show_list/<int:x>', methods=['GET'])
+#def getList(x):
+#   Book.query.limit(x).all()
+
+#     def __init__ (self, id, isbn, name, description, price, genre, publisher, year_published, copies_sold, author_id):
+#         self.id = id
+#         self.isbn= isbn
+#         self.name = name
+#         self.description = description
+#         self.price = price
+#         self. genre = genre
+#         self. publisher = publisher
+#         self.year_published = year_published
+#         self.copies_sold = copies_sold
+#         self.author_id = author_id
+#
+#class BookSchema(ma.Schema):
+#     class Meta:
+#         fields = ('isbn', 'name', 'description', 'price', 'genre', 'publisher', 'year_published', 'copies_sold')
+
     def __repr__(self):
         return f"Book('{self.name}', '{self.isbn}', '{self.price}', '{self.author}','{self.genre}')"
+
+book_schema = BookSchema(strict=True)
+books_schema = BookSchema(many=True, strict=True)
+
+
+
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- manage.py
+++ manage.py
@@ -1,15 +1,23 @@
 import os
+
+from flask import jsonify
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from bookapp import create_app, db
-from bookapp.models import users
+from bookapp.models import users, Book
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
+
+@app.route('/book', methods=['GET'])
+def get_book():
+    all_book = Book.query.all()
+    result = book.dump(all_book)
+    return jsonify(result.data)
 
 if __name__ == '__main__':
     manager.run()
